f109beaab8f375a0b6d09f6cc73efa8d595f09e9
Adam Malec <add.malec@gmail.com>
2025-04-03 09:28:43 +0300

    fix(crutch): add hidden images for extra-services section

--- src/js/extra-services.js
+++ src/js/extra-services.js
@@ -4,7 +4,7 @@ const extraServices = document.querySelectorAll(".extra__item");
 const extraShowcase = document.querySelector(".showcase__bg");
 
 function changeShowcaseImage(i) {
-  extraShowcase.style.backgroundImage = `url('./src/img/extra-services-${i + 1}.jpg')`;
+  extraShowcase.style.backgroundImage = `url('/dianacome/assets/extra-services-${i + 1}.jpg')`;
 }
 
 for (const [i, service] of extraServices.entries()) {
